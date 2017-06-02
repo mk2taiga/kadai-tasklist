af217385b7eb42efb442183432b054fa23d2f9ce
Taiga Miyazaki <mk2_tg000@icloud.com>
2017-06-02 11:25:09 +0000

    index fix

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -13,9 +13,11 @@
             </thead>
             <tbody>
                 @foreach ($tasks as $task)
-                    <td>{!! link_to_route('tasks.show', $task->id, ['id' => $task->id]) !!}</td>
-                    <td>{{ $task->status }}</td>
-                    <td>{{ $task->content }}</td>
+                    <tr>
+                        <td>{!! link_to_route('tasks.show', $task->id, ['id' => $task->id]) !!}</td>
+                        <td>{{ $task->status }}</td>
+                        <td>{{ $task->content }}</td>
+                    </tr>
                 @endforeach
             </tbody>
         </table>
